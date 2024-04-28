33675c2967be3d929220350e32769e09a7ede412
matiliendogit <matiliendodev@gmail.com>
2024-04-28 12:31:56 -0300

    Practice .Net Form level 2

---
+++ Form1.Designer.cs
@@ -0,0 +1,225 @@
+﻿namespace Practica5
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblApellido = new System.Windows.Forms.Label();
+            this.lblNombre = new System.Windows.Forms.Label();
+            this.lblEdad = new System.Windows.Forms.Label();
+            this.lblDireccion = new System.Windows.Forms.Label();
+            this.lblResultado = new System.Windows.Forms.Label();
+            this.tboxApellido = new System.Windows.Forms.TextBox();
+            this.tboxNombre = new System.Windows.Forms.TextBox();
+            this.tboxEdad = new System.Windows.Forms.TextBox();
+            this.tboxDireccion = new System.Windows.Forms.TextBox();
+            this.listBoxResultado = new System.Windows.Forms.ListBox();
+            this.btnAceptar = new System.Windows.Forms.Button();
+            this.btnCancelar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblApellido
+            // 
+            this.lblApellido.AutoSize = true;
+            this.lblApellido.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblApellido.Location = new System.Drawing.Point(58, 31);
+            this.lblApellido.Name = "lblApellido";
+            this.lblApellido.Size = new System.Drawing.Size(79, 18);
+            this.lblApellido.TabIndex = 0;
+            this.lblApellido.Text = "APELLIDO";
+            // 
+            // lblNombre
+            // 
+            this.lblNombre.AutoSize = true;
+            this.lblNombre.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblNombre.Location = new System.Drawing.Point(58, 73);
+            this.lblNombre.Name = "lblNombre";
+            this.lblNombre.Size = new System.Drawing.Size(75, 18);
+            this.lblNombre.TabIndex = 1;
+            this.lblNombre.Text = "NOMBRE";
+            // 
+            // lblEdad
+            // 
+            this.lblEdad.AutoSize = true;
+            this.lblEdad.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblEdad.Location = new System.Drawing.Point(58, 116);
+            this.lblEdad.Name = "lblEdad";
+            this.lblEdad.Size = new System.Drawing.Size(49, 18);
+            this.lblEdad.TabIndex = 2;
+            this.lblEdad.Text = "EDAD";
+            // 
+            // lblDireccion
+            // 
+            this.lblDireccion.AutoSize = true;
+            this.lblDireccion.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblDireccion.Location = new System.Drawing.Point(58, 159);
+            this.lblDireccion.Name = "lblDireccion";
+            this.lblDireccion.Size = new System.Drawing.Size(91, 18);
+            this.lblDireccion.TabIndex = 3;
+            this.lblDireccion.Text = "DIRECCION";
+            // 
+            // lblResultado
+            // 
+            this.lblResultado.AutoSize = true;
+            this.lblResultado.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblResultado.Location = new System.Drawing.Point(58, 203);
+            this.lblResultado.Name = "lblResultado";
+            this.lblResultado.Size = new System.Drawing.Size(99, 18);
+            this.lblResultado.TabIndex = 4;
+            this.lblResultado.Text = "RESULTADO";
+            // 
+            // tboxApellido
+            // 
+            this.tboxApellido.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tboxApellido.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tboxApellido.Location = new System.Drawing.Point(178, 28);
+            this.tboxApellido.Name = "tboxApellido";
+            this.tboxApellido.Size = new System.Drawing.Size(460, 27);
+            this.tboxApellido.TabIndex = 5;
+            // 
+            // tboxNombre
+            // 
+            this.tboxNombre.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tboxNombre.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tboxNombre.Location = new System.Drawing.Point(178, 69);
+            this.tboxNombre.Name = "tboxNombre";
+            this.tboxNombre.Size = new System.Drawing.Size(460, 27);
+            this.tboxNombre.TabIndex = 6;
+            // 
+            // tboxEdad
+            // 
+            this.tboxEdad.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tboxEdad.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tboxEdad.Location = new System.Drawing.Point(178, 113);
+            this.tboxEdad.Name = "tboxEdad";
+            this.tboxEdad.Size = new System.Drawing.Size(62, 27);
+            this.tboxEdad.TabIndex = 7;
+            // 
+            // tboxDireccion
+            // 
+            this.tboxDireccion.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tboxDireccion.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tboxDireccion.Location = new System.Drawing.Point(178, 152);
+            this.tboxDireccion.Name = "tboxDireccion";
+            this.tboxDireccion.Size = new System.Drawing.Size(460, 27);
+            this.tboxDireccion.TabIndex = 8;
+            // 
+            // listBoxResultado
+            // 
+            this.listBoxResultado.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.listBoxResultado.FormattingEnabled = true;
+            this.listBoxResultado.ItemHeight = 16;
+            this.listBoxResultado.Location = new System.Drawing.Point(61, 239);
+            this.listBoxResultado.Name = "listBoxResultado";
+            this.listBoxResultado.Size = new System.Drawing.Size(577, 148);
+            this.listBoxResultado.TabIndex = 9;
+            // 
+            // btnAceptar
+            // 
+            this.btnAceptar.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
+            this.btnAceptar.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnAceptar.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.btnAceptar.FlatAppearance.BorderSize = 5;
+            this.btnAceptar.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(128)))));
+            this.btnAceptar.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.btnAceptar.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAceptar.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.btnAceptar.Location = new System.Drawing.Point(220, 406);
+            this.btnAceptar.Name = "btnAceptar";
+            this.btnAceptar.Size = new System.Drawing.Size(85, 35);
+            this.btnAceptar.TabIndex = 10;
+            this.btnAceptar.Text = "Aceptar";
+            this.btnAceptar.UseVisualStyleBackColor = true;
+            this.btnAceptar.Click += new System.EventHandler(this.btnAceptar_Click);
+            // 
+            // btnCancelar
+            // 
+            this.btnCancelar.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
+            this.btnCancelar.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnCancelar.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.btnCancelar.FlatAppearance.BorderSize = 5;
+            this.btnCancelar.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(128)))));
+            this.btnCancelar.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.btnCancelar.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCancelar.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.btnCancelar.Location = new System.Drawing.Point(380, 406);
+            this.btnCancelar.Name = "btnCancelar";
+            this.btnCancelar.Size = new System.Drawing.Size(85, 35);
+            this.btnCancelar.TabIndex = 11;
+            this.btnCancelar.Text = "Cancelar";
+            this.btnCancelar.UseVisualStyleBackColor = true;
+            this.btnCancelar.Click += new System.EventHandler(this.btnCancelar_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoValidate = System.Windows.Forms.AutoValidate.EnableAllowFocusChange;
+            this.BackColor = System.Drawing.Color.OldLace;
+            this.ClientSize = new System.Drawing.Size(682, 453);
+            this.Controls.Add(this.btnCancelar);
+            this.Controls.Add(this.btnAceptar);
+            this.Controls.Add(this.listBoxResultado);
+            this.Controls.Add(this.tboxDireccion);
+            this.Controls.Add(this.tboxEdad);
+            this.Controls.Add(this.tboxNombre);
+            this.Controls.Add(this.tboxApellido);
+            this.Controls.Add(this.lblResultado);
+            this.Controls.Add(this.lblDireccion);
+            this.Controls.Add(this.lblEdad);
+            this.Controls.Add(this.lblNombre);
+            this.Controls.Add(this.lblApellido);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Datos Personales";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblApellido;
+        private System.Windows.Forms.Label lblNombre;
+        private System.Windows.Forms.Label lblEdad;
+        private System.Windows.Forms.Label lblDireccion;
+        private System.Windows.Forms.Label lblResultado;
+        private System.Windows.Forms.TextBox tboxApellido;
+        private System.Windows.Forms.TextBox tboxNombre;
+        private System.Windows.Forms.TextBox tboxEdad;
+        private System.Windows.Forms.TextBox tboxDireccion;
+        private System.Windows.Forms.ListBox listBoxResultado;
+        private System.Windows.Forms.Button btnAceptar;
+        private System.Windows.Forms.Button btnCancelar;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Practica5
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnAceptar_Click(object sender, EventArgs e)
+        {
+            string apellido = tboxApellido.Text.ToUpper();
+            string nombre = tboxNombre.Text.ToUpper();
+            int edad;
+            string direccion = tboxDireccion.Text.ToUpper();
+
+            List<string> errores = new List<string>();
+
+            if (string.IsNullOrEmpty(apellido))
+            {
+                tboxApellido.BackColor = Color.Red;
+                tboxApellido.Focus();
+                errores.Add(apellido);
+            }
+            else if (apellido.Length > 30 || apellido.Length < 2)
+            {
+                tboxApellido.BackColor = Color.Red;
+                tboxApellido.Focus();
+                MessageBox.Show("Debe tener entre 2 y 30 caracteres");
+                errores.Add(apellido);
+
+            }
+            else
+            {
+                errores.Remove(apellido);
+                tboxApellido.BackColor = SystemColors.Window;
+            }
+
+            if (string.IsNullOrEmpty(nombre))
+            {
+                tboxNombre.BackColor = Color.Red;
+                tboxNombre.Focus();
+                errores.Add((nombre));
+            }
+            else if (nombre.Length > 30 || nombre.Length < 2)
+            {
+                tboxNombre.BackColor = Color.Red;
+                tboxNombre.Focus();
+                MessageBox.Show("Debe tener entre 2 y 30 caracteres");
+                errores.Add((nombre));
+
+            }
+            else
+            {
+                errores.Remove((nombre));
+                tboxNombre.BackColor = SystemColors.Window;
+            }
+
+            if (string.IsNullOrEmpty(tboxEdad.Text))
+            {
+                tboxEdad.BackColor = Color.Red;
+                tboxEdad.Focus();
+                errores.Add(tboxEdad.Text);
+            }
+            else if (!int.TryParse(tboxEdad.Text, out edad))
+            {
+                MessageBox.Show("Por favor ingrese una edad válida");
+                errores.Add(tboxEdad.Text);
+                tboxEdad.Focus();
+                return;
+            } else
+            {
+                errores.Remove(tboxEdad.Text);
+                tboxEdad.BackColor = SystemColors.Window;
+            }
+
+            if (string.IsNullOrEmpty(direccion))
+            {
+                tboxDireccion.BackColor = Color.Red;
+                tboxDireccion.Focus();
+                errores.Add(direccion);
+            }
+            else if (direccion.Length > 50 || direccion.Length < 10)
+            {
+                tboxDireccion.BackColor = Color.Red;
+                tboxDireccion.Focus();
+                MessageBox.Show("Debe tener entre 10 y 50 caracteres");
+                errores.Add(direccion);
+
+            }
+            else 
+            {
+                errores.Remove(direccion);
+                tboxDireccion.BackColor = SystemColors.Window; 
+            }
+
+            if (errores.Count != 0) 
+            { 
+                listBoxResultado.Items.Clear();
+            }else
+            {
+                listBoxResultado.Items.Add("Nombre y apellido : " + nombre + " " + apellido);
+                listBoxResultado.Items.Add("Edad: " + tboxEdad.Text);
+                listBoxResultado.Items.Add("Dirección: " + direccion);
+            }
+            
+        }
+
+        private void btnCancelar_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
